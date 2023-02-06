6e7dab95c11012cd9e703383820f39c9e17a2299
Sergey <sergeychikin1981@gmail.com>
2023-02-06 22:13:40 +0300

    Исправил техническую часть курсовой работы. Создал класс Employee в отдельном файле.

--- src/Main.java
+++ src/Main.java
@@ -103,48 +103,5 @@ public class Main {
     }
 
 }
-class Employee {
-    private String fullName;
-    private int department;
-    private int salary;
-    private int id = 0;
-    public Employee(String fullName, int department, int salary){
-        this.fullName = fullName;
-        this.department = department;
-        this.salary = salary;
-        id = Main.counterId();
-    }
-
-    public String getFullName() {
-        return this.fullName;
-    }
-
-    public int getDepartment() {
-        return this.department;
-    }
-
-    public int getSalary() {
-        return this.salary;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public void setDepartment(byte department) {
-        this.department = department;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "Ф.И.О - " + fullName + "; Индекс - " + String.valueOf(getId()) + "; Отдел - " + department +
-                "; Зарплата - " + salary + " рублей.";
-    }
-
-}
 
 
